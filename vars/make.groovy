cb207f176f2b031be5e54cfa823fc4217ba1c365
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-08-15 13:09:04 +0800

    modify make

--- vars/make.groovy
+++ vars/make.groovy
@@ -1,17 +1,49 @@
 def call(String type) {
 	switch(type) {
 		case 'build':
-			return {
-				parallel (
-					'stage 1 build': {
-						echo 'stage 1 build'
-					},
-					'stage 2 build': {
-						echo 'stage 2 build'
-					}
-				)
-			}
+			def arch = [x86: false, arm: false]
+		    def configs = readYaml file: 'config.yml'
+		    def services = configs.services
+
+		    services.each { s ->
+		        def serviceArch = s.getAt('arch')
+		        if (serviceArch == 'arm' && !arch.arm) {
+		            arch.arm = true
+		        } else if (serviceArch == 'x86' && !arch.x86) {
+		            arch.x86 = true
+		        }
+		    }
+
+		    if (arch.arm && !arch.x86) {
+		    	return {
+					parallel (
+						'arm': {
+							echo 'do build arm'
+						}
+					)
+				}
+		    } else if (!arch.arm && arch.x86) {
+		    	return {
+					parallel (
+						'x86': {
+							echo 'do build x86'
+						}
+					)
+				}
+	    	} else {
+	    		return {
+					parallel (
+						'arm': {
+							echo 'do build arm'
+						},
+						'x86': {
+							echo 'do build x86'
+						}
+					)
+				}
+	    	}
 			break
+		
 		case 'deploy':
 			return {
 				parallel (
