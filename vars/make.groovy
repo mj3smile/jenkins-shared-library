61f70b6ee9f85ad024710cff416ba93a2a7dccdc
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-08-15 12:46:16 +0800

    modify make

--- vars/make.groovy
+++ vars/make.groovy
@@ -1,27 +1,27 @@
 def call(String type) {
 	switch(type) {
 		case 'build':
-			script {
-				parallel (
+			return {
+				[
 					'stage 1 build': {
 						echo 'stage 1 build'
 					},
 					'stage 2 build': {
 						echo 'stage 2 build'
 					}
-				).call()
+				]
 			}
 			break
 		case 'deploy':
-			script {
-				parallel (
+			return {
+				[
 					'stage 1 deploy': {
 						echo 'stage 1 deploy'
 					},
 					'stage 2 deploy': {
 						echo 'stage 2 deploy'
 					}
-				).call()
+				]
 			}
 			break
 	}
