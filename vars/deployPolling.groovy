4401d317106bdc220003e4fd7417e8af3d242542
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-09-01 12:28:50 +0800

    add credential load in deployment steps

--- vars/deployPolling.groovy
+++ vars/deployPolling.groovy
@@ -1,3 +1,6 @@
 def call() {
 	echo "do deploy to polling cluster."
+	withCredentials([string(credentialsId: 'ENV_GPG_PASSPHRASE', variable: 'ENV_GPG_PASSPHRASE')]) {
+      sh 'python3 env.py'
+    }
 }
