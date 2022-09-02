d8ecc3fa3ded68d9b5a32acd14c5de7cdb39259a
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-09-02 15:20:28 +0800

    modify deployment steps

--- vars/deployDevelopment.groovy
+++ vars/deployDevelopment.groovy
@@ -1,3 +1,3 @@
 def call() {
-	sh 'ssh mj@127.0.0.1 \'cd /home/mj/jenkins/workspace/shared-library-tr && export ENV_GPG_PASSPHRASE=$ENV_GPG_PASSPHRASE && forstok deploy --cluster development\''
+	sh 'ssh mj@127.0.0.1 \'cd /home/mj/jenkins/workspace/shared-library-tr && export ENV_GPG_PASSPHRASE=$(echo $ENV_FILE) && forstok deploy --cluster development\''
 }
