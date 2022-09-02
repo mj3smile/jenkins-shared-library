23772c9a19417dad147a09bd4b5d4a34ac74f43e
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-09-02 15:21:49 +0800

    modify deployment steps

--- vars/deployApi.groovy
+++ vars/deployApi.groovy
@@ -1,3 +1,3 @@
 def call() {
-	sh 'ssh mj@127.0.0.1 \'cd /home/mj/jenkins/workspace/shared-library-tr && export ENV_GPG_PASSPHRASE=$(echo $ENV_FILE) && forstok deploy --cluster api\''
+	sh 'ssh mj@127.0.0.1 \'cd /home/mj/jenkins/workspace/shared-library-tr && export ENV_GPG_PASSPHRASE=$(echo $ENV_GPG_PASSPHRASE) && forstok deploy --cluster api\''
 }
--- vars/deployPolling.groovy
+++ vars/deployPolling.groovy
@@ -1,3 +1,3 @@
 def call() {
-	sh 'ssh mj@127.0.0.1 \'cd /home/mj/jenkins/workspace/shared-library-tr && export ENV_GPG_PASSPHRASE=$(echo $ENV_FILE) && forstok deploy --cluster polling\''
+	sh 'ssh mj@127.0.0.1 \'cd /home/mj/jenkins/workspace/shared-library-tr && export ENV_GPG_PASSPHRASE=$(echo $ENV_GPG_PASSPHRASE) && forstok deploy --cluster polling\''
 }
